d187416101211f749da38a2835d56ad03c43ff46
Aparna Chatterjee <aparna.c.work@zohomail.in>
2026-02-01 18:19:58 +0530

    Committing

--- git-service/src/main/java/in/maven/ark/lms/git/service/GitHubService.java
+++
@@ -1,8 +0,0 @@
-package in.maven.ark.lms.git.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class GitHubService {
-    // Placeholder for GitHub specific operations
-}
--- git-service/src/main/java/in/maven/ark/lms/git/service/GitLabService.java
+++
@@ -1,8 +0,0 @@
-package in.maven.ark.lms.git.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class GitLabService {
-    // Placeholder for GitLab specific operations
-}
